9160808d2de30610c42c92bace9897cc4cee9665
lizhixiang <lizhixiang@egova.com.cn>
2019-09-05 12:25:03 +0800

    [M]添加发送邮箱验证的模块

--- src/lib/util.js
+++ src/lib/util.js
@@ -1,20 +1,54 @@
+const nodemailer = require('nodemailer');
+const Mailgen = require('mailgen');
 
-/**	Creates a callback that proxies node callback style arguments to an Express Response object.
- *	@param {express.Response} res	Express HTTP Response
- *	@param {number} [status=200]	Status code to send on success
- *
- *	@example
- *		list(req, res) {
- *			collection.find({}, toRes(res));
- *		}
- */
-export function toRes(res, status=200) {
-	return (err, thing) => {
-		if (err) return res.status(500).send(err);
+const mailGenerator = new Mailgen({
+	theme: 'default',
+	product: {
+		name: 'Mailgen',
+		link: 'https://mailgen.js/'
+	}
+});
 
-		if (thing && typeof thing.toObject==='function') {
-			thing = thing.toObject();
+const email = {
+	body: {
+		name: 'John Appleseed',
+		intro: 'You have received this email because a password reset request for your account was received.',
+		action: {
+			instructions: 'Click the button below to reset your password:',
+			button: {
+				color: '#DC4D2F',
+				text: 'Reset your password',
+				link: 'https://mailgen.js/reset?s=b350163a1a010d9729feb74992c1a010'
+			}
+		},
+		outro: 'If you did not request a password reset, no further action is required on your part.'
+	}
+};
+
+const emailBody = mailGenerator.generate(email);
+
+
+// 发送邮件的工具函数
+export function sendMail(options) {
+	let testAccount = nodemailer.createTestAccount();
+
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.ethereal.email',
+		port: 587,
+		secure: false,
+		auth: {
+			user: testAccount.user,
+			pass: testAccount.pass
 		}
-		res.status(status).json(thing);
-	};
+	});
+
+	transporter.sendMail({
+		from: '"Fred Foo 👻" <foo@example.com>',
+		to: 'bar@example.com, baz@example.com',
+		subject: 'Hello ✔',
+		text: 'Hello world?',
+		html: '<b>Hello world?</b>'
+	});
 }
+
+
